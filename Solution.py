7d6f311a67396d87cd198248486332707f5bab4c
Saipritamr5603 <119448595+Saipritamr5603@users.noreply.github.com>
2022-11-29 11:18:33 -0800

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -47,4 +47,32 @@ def merge_sort(arr,cmp):
              k += 1 
 # must be in-place sort
 def quick_sort(arr,cmp):
-    pass
+    if len(arr) > 1: 
+         # partition array 
+         pivot = arr[0] 
+         left = [] 
+         right = [] 
+         for i in range(1, len(arr)): 
+             c = cmp(arr[i], pivot) 
+             if c < 0: 
+                 left.append(arr[i]) 
+             if c > 0: 
+                 right.append(arr[i]) 
+
+         quick_sort(left, cmp) 
+         quick_sort(right, cmp) 
+
+         n = len(arr) 
+         n1 = len(left) 
+         n2 = len(right) 
+         arr[:n1] = left 
+         arr[n1:n-n2] = [pivot] * (n - n1 - n2) 
+         arr[n-n2:] = right 
+
+ def cmp(a, b): 
+     if a < b: 
+         return -1 
+     elif a == b: 
+         return 0 
+     else: 
+         return 1
